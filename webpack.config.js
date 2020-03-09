2e5ddf4ced78a7015523ff59a6b57725e38e3455
fanmu <fanmucc@yeah.net>
2020-03-09 15:25:02 +0800

    个人网站初始化

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        progress: true,
+        port: 4000
+    }
+}
